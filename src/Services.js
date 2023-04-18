27c8006564c3925ed8e7c43a7252c1ab4deb8136
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-04-18 14:15:40 +0530

    Add files via upload

--- src/Services.js
+++ src/Services.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ServiceCard from './component/ServiceCard';
 
 const Service = () => {
 
@@ -9,44 +10,7 @@ const Service = () => {
           <p className='common-subheading mb-2'>Services</p>
           <h1>What I DO</h1>
         </div>
-        <div className="card-container grid grid-two-column ">
-          <div className="card service-card">
-            <div className="card-body">
-              <div className="icon my-4">
-                <i className="fa-solid fa-laptop-code"></i>
-              </div>
-              <h1 className='my-4'>Web Development</h1>
-              <p className="hero-para my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea animi amet, perferendis vel error sapiente. Et repudiandae voluptatibus sapiente.</p>
-            </div>
-          </div>
-          <div className="card service-card">
-            <div className="card-body">
-              <div className="icon my-4">
-                <i className="fa-solid fa-mobile-screen"></i>
-              </div>
-              <h1 className='my-4'>Mobile Design</h1>
-              <p className="hero-para my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea animi amet, perferendis vel error sapiente. Et repudiandae voluptatibus sapiente.</p>
-            </div>
-          </div>
-          <div className="card service-card">
-            <div className="card-body">
-              <div className="icon my-4">
-                <i className="fa-brands fa-figma"></i>
-              </div>
-              <h1 className='my-4'>Figma Design</h1>
-              <p className="hero-para my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea animi amet, perferendis vel error sapiente. Et repudiandae voluptatibus sapiente.</p>
-            </div>
-          </div>
-          <div className="card service-card">
-            <div className="card-body">
-              <div className="icon my-4">
-                <i className="fa-solid fa-database"></i>
-              </div>
-              <h1 className='my-4'>Rest API</h1>
-              <p className="hero-para my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea animi amet, perferendis vel error sapiente. Et repudiandae voluptatibus sapiente.</p>
-            </div>
-          </div>
-        </div>
+        <ServiceCard />
       </section>
     </>
   )
