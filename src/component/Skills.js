ee53ec026deab7a33ee61c97204f7554c25f2db8
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-04-18 14:14:03 +0530

    Update Skills.js

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -13,7 +13,8 @@ const Skills = () => {
                     <span className='skill'>JavaScript</span>
                     <span className='skill'>ReactJS</span>
                     <span className='skill'>Firebase</span>
-                    <span className='skill'>Git</span>
+                    <span className='skill'>PHP</span>
+                    <span className='skill'>MySQL</span>
                 </div>
             </div>
         </>
