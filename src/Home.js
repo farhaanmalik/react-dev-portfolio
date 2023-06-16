647fc01c6251bf93750077ef0e935d76f0272f89
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-06-16 17:58:55 +0530

    Update Home.js
    Content change

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import Contact from './Contact'
 const Home = () => {
 
   const data = {
-    subhead: "Assalamu'alaikum, I'm ",
+    subhead: "Hey There! I am",
     name: "Farhaan Malik",
     myRole: "Frontend Developer",
     myInfo: "Building & Maintaining responsive websites. Exploring new Web designs. Converting Figma design into HTML Code.",
