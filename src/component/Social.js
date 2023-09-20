16e22d43a11f9970a4d89d90c32141c0931231c7
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-09-20 09:01:42 +0530

    Updated Telegram link

--- src/component/Social.js
+++ src/component/Social.js
@@ -5,7 +5,7 @@ const Social = () => {
         <>
             <div className="slinks d-flex mt-4 pt-2">
                 <div className="link">
-                    <a href="https://t.me/frontdevlp" target="_blank" rel='noreferrer'><i className="fa-brands fa-telegram"></i></a>
+                    <a href="https://t.me/farhaan005" target="_blank" rel='noreferrer'><i className="fa-brands fa-telegram"></i></a>
                 </div>
                 <div className="link">
                     <a href="https://www.linkedin.com/in/farhaan-malik-1b4022230/" target="_blank" rel='noreferrer'><i className="fa-brands fa-linkedin"></i></a>
