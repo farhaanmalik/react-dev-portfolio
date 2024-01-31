86255292da2c0038556612806e42b97bfcab5c70
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2024-01-31 19:19:15 +0530

    Added Project

--- src/ProjectAPI.js
+++ src/ProjectAPI.js
@@ -1,64 +1,57 @@
 const Project = [
     {
     id: 1,
+    image: "./images/realEstate.png",
+    projectName: "Real Estate - React",
+    projectLink:"https://primeview-realestate.vercel.app/",
+    
+},
+    {
+    id: 2,
     image: "./images/p-9.png",
     projectName: "Simple Quiz App",
     projectLink:"https://codingquizapp.vercel.app/",
     
 },
     {
-    id: 2,
+    id: 3,
     image: "./images/p-10.png",
     projectName: "React - Weather App",
     projectLink:"https://liveweather-app.vercel.app/",
     
 },
     {
-    id: 3,
+    id: 4,
     image: "./images/p-8.png",
     projectName: "Sharepire - Blogging Community",
     projectLink:"https://sharepire.dev/",
     
 },
     {
-    id: 4,
+    id: 5,
     image: "./images/p-1.png",
     projectName: "Live Weather App",
     projectLink:"https://farhaanmalik.github.io/weather-app/",
     
 },
 {
-    id: 5,
+    id: 6,
     image: "./images/p-2.png",
     projectName: "Community Web Design",
     projectLink:"https://farhaanmalik.github.io/bloggingcommunity-Uidesign/",
 },
 {
-    id: 6,
+    id: 7,
     image: "./images/p-3.png",
     projectName: "Portfolio Using HTML/CSS",
     projectLink:" https://farhaanmalik.github.io/PersonalPortFolio/",
     
 },
 {
-    id: 7,
+    id: 8,
     image: "./images/p-4.png",
     projectName: "Abdulxamill Dev Team",
     projectLink:"https://farhaanmalik.github.io/Abdulxamill-Dev-Team/",
-},
-{
-    id: 8,
-    image: "./images/p-7.png",
-    projectName: "To-Do List WebApp",
-    projectLink:"https://farhaanmalik.github.io/Simple-To-Do-List/",
-    
-},
-{
-    id: 9,
-    image: "./images/p-6.png",
-    projectName: "Note WebApp",
-    projectLink:"https://farhaanmalik.github.io/Simple-NotePad/",
-    
 },
 ];
 
