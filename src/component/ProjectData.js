93d5b8dc6e49f9b0a14da83d84e8bc398368a42a
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2024-02-13 19:18:39 +0530

    bug solved

--- src/component/ProjectData.js
+++ src/component/ProjectData.js
@@ -4,54 +4,54 @@ const Project = [
     image: "./images/realEstate.png",
     projectName: "Real Estate - React",
     projectLink:"https://primeview-realestate.vercel.app/",
-    
+
 },
     {
     id: 2,
-    image: "./images/p-8.png",
-    projectName: "Sharepire - Blogging Community",
-    projectLink:"https://sharepire.dev/",
-    
+    image: "./images/p-9.png",
+    projectName: "Simple Quiz App",
+    projectLink:"https://codingquizapp.vercel.app/",
+
 },
     {
     id: 3,
+    image: "./images/p-10.png",
+    projectName: "React - Weather App",
+    projectLink:"https://liveweather-app.vercel.app/",
+
+},
+    {
+    id: 4,
+    image: "./images/p-8.png",
+    projectName: "Sharepire - Blogging Community",
+    projectLink:"https://sharepire.dev/",
+
+},
+    {
+    id: 5,
     image: "./images/p-1.png",
     projectName: "Live Weather App",
     projectLink:"https://farhaanmalik.github.io/weather-app/",
-    
-},
-{
-    id: 4,
-    image: "./images/p-2.png",
-    projectName: "Community Web Design",
-    projectLink:"#",
-},
-{
-    id: 5,
-    image: "./images/p-3.png",
-    projectName: "Portfolio Using HTML/CSS",
-    projectLink:" https://farhaanmalik.github.io/PersonalPortFolio/",
-    
+
 },
 {
     id: 6,
-    image: "./images/p-4.png",
-    projectName: "Abdulxamill Dev Team",
-    projectLink:"https://farhaanmalik.github.io/Abdulxamill-Dev-Team/",
+    image: "./images/p-2.png",
+    projectName: "Community Web Design",
+    projectLink:"https://farhaanmalik.github.io/bloggingcommunity-Uidesign/",
 },
 {
     id: 7,
-    image: "./images/p-7.png",
-    projectName: "To-Do List WebApp",
-    projectLink:"https://farhaanmalik.github.io/Simple-To-Do-List/",
-    
+    image: "./images/p-3.png",
+    projectName: "Portfolio Using HTML/CSS",
+    projectLink:" https://farhaanmalik.github.io/PersonalPortFolio/",
+
 },
 {
     id: 8,
-    image: "./images/p-6.png",
-    projectName: "Note WebApp",
-    projectLink:"https://farhaanmalik.github.io/Simple-NotePad/",
-    
+    image: "./images/p-4.png",
+    projectName: "Abdulxamill Dev Team",
+    projectLink:"https://farhaanmalik.github.io/Abdulxamill-Dev-Team/",
 },
 ];
 
